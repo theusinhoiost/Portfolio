0c1d25deedc0021eece73d074a6f6fbaab3d731b
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2023-08-24 02:35:53 -0300

    auto year

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const footer = document.querySelector('.addYear');
+const text = `© Matheus Luiz Iost ${getYear()}. MIT License.`;
+const p = document.createElement('p');
+p.textContent = text;
+footer.appendChild(p)
+
+
+function getYear(){
+   let date = new Date();
+    let year = date.getFullYear();
+    return year;
+
+}
+
