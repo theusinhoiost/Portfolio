fee087f41a16263e343d670ef6620866684e99a9
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2024-01-17 02:50:13 -0300

    add the NFW project

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const footer = document.querySelector('.addYear');
 const text = `© Matheus Luiz Iost ${date}. MIT License.`;
 const p = document.createElement('p');
 p.textContent = text;
-footer.appendChild(p)
+footer.appendChild(p);
 
 
 
